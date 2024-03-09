5d2bd05b4080ca3726e99b049dc144e067d598f1
BartoszOw <owbartosz.pl@gmail.com>
2024-03-09 18:18:51 +0100

    Changes

--- forms.py
+++ forms.py
@@ -6,20 +6,20 @@ class ExpensesForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     quantity = FloatField('quantity', validators=[DataRequired()])
     type = SelectField('type', validators=[DataRequired()],
-                    choices=[(1, 'Artykuły spożywcze'), 
-                             (2, 'Rozrywka'),
-                             (3, 'Technologia'),
-                             (4, 'Zdrowie'),
-                             (5, 'Uzależnienie'),
-                             (6, 'Przedmioty użytku codziennego'),
-                             (7, 'Praca')] 
+                    choices=[('Artykuły spożywcze', 'Artykuły spożywcze'), 
+                             ('Rozrywka', 'Rozrywka'),
+                             ('Technologia', 'Technologia'),
+                             ('Zdrowie', 'Zdrowie'),
+                             ('Uzależnienie', 'Uzależnienie'),
+                             ('Przedmioty użytku codziennego', 'Przedmioty użytku codziennego'),
+                             ('Praca', 'Praca')] 
                       )
 
 class IncomeForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     quantity = FloatField('quantity', validators=[DataRequired()])
     type = SelectField('type', validators=[DataRequired()],
-                    choices=[(1, 'Praca'), 
-                             (2, 'Inwestycje'),
-                             (3, 'Prezent')] 
+                    choices=[('Praca', 'Praca'), 
+                             ('Inwestycje', 'Inwestycje'),
+                             ('Prezent', 'Prezent')] 
                       )
